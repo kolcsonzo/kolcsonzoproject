1115d1828d8c77c18f596e1abbf1d339ca082082
L3GA911 <lengyelgabor7@gmail.com>
2020-03-12 15:34:57 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,9 +6,15 @@
 <head>
 <meta charset="utf-8">
 <title>Bejelentkezés - Eszköznyilvántartó és kölcsönző rendszer</title>
-	<script src="https://kit.fontawesome.com/cc6376bd80.js" crossorigin="anonymous"></script>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://fonts.googleapis.com/css?family=Lato:400,400i|PT+Sans|Roboto&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/cc6376bd80.js" crossorigin="anonymous"></script>
+
 	<link rel="stylesheet" href="styles/login_styles.css">
 </head>
 <body>
@@ -52,8 +58,11 @@ echo '
             header("Location: foglalas.php");
         } else {
 			//Hibaüzenet
-            echo '<h4 style="color:#f19999">Hibás felhasználónév vagy jelszó!</h4><br/>';
-        }
+            echo 	'<div class="alertpos alert alert-danger alert-dismissible fade show">
+					<button type="button" class="close" data-dismiss="alert">&times;</button>
+					<strong>Figyelem!</strong> Hibás felhasználónév vagy jelszó!
+					</div>';
+				}
     }
 ?>
 			</article>
