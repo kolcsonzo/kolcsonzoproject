087a571618ea3f664972d9c65109e5f9120eb028
MaXing <hsroreloaded@gmail.com>
2020-03-22 23:34:02 +0100

    Foglalás kb 70%

--- foglalas.php
+++ foglalas.php
@@ -5,57 +5,32 @@
 	header("Content-Type: text/html; charset=utf-8");
 	include('inc/top.php');
 ?>
+
 <main>
 	<div class="pagename" style>
 		<span class="text content-name">Eszköz foglalása</span>
 	</div>
 	<form id="reservingForm" action="/action_page.php">
-	  <!-- Lépések -->
-	  <div class="tab">
+	  <?php
+		echo '
+		<div class="tab">
 			<label for="">Eszköz:</label><br>
-			<select id="" name="" class="select">
-				<option value="1">Laptop</option>
-				<option value="2">Monitor</option>
+			<select onchange="foglalas(document.getElementById('."'eszkoz'".').value,'."'na'".');" id="eszkoz" name="" class="select">
+			<option value="" disabled selected>Kérlek válassz!</option>';
+				$query = "SELECT name FROM `devices` GROUP BY name";
+				$result = mysqli_query($con, $query) or die(mysql_error());
+				while ($row = $result->fetch_assoc()) {
+				echo '
+				<option value="'.$row["name"].'">'.$row["name"].'</option>
+				';
+				}
+		echo '
 			</select>
-			<label for="">Szabad eszközök listázása:</label><br>
-			<label class="switch">
-				<input type="checkbox">
-				<span class="slider"></span>
-			</label>
-	  </div>
-	  <div class="tab">
-			<label for="">Eszköz típusa:</label><br>
-			<select id="" name="" class="select">
-				<option value="1">Típus1</option>
-				<option value="2">Típus2</option>
-			</select>
-	  </div>
-	  <div class="tab">
-		<label for="">Foglalás időtartama:</label><br>
-			<select id="" name="" class="select">
-				<option value="1">1</option>
-				<option value="2">2</option>
-				<option value="3">3</option>
-			</select>
-	  </div>
-	  <div class="tab">
-		<label for="">Foglalás összegzése:</label><br>
-		
-	  </div><br><br>
-		<center>
-		  <button type="button" id="prevBtn" onclick="nextPrev(-1)" class="button">Előző</button>
-		  <button type="button" id="nextBtn" onclick="nextPrev(1)"class="button">Következő</button>
-		</center>
-	  <!-- Körök -->
-	  <div style="text-align:center;">
-		<br>
-		<span class="step"></span>
-		<span class="step"></span>
-		<span class="step"></span>
-		<span class="step"></span>
-	  </div>
+	  </div>';
+	  ?>
+	  <div id="foglalas_result" class=""></div>
 	</form>
-	<script type="text/javascript" src="js/reserving_form.js"></script>
+
 </main>
 <?php
 	include('inc/bottom.php');
---
+++ foglalas_query.php
@@ -0,0 +1,53 @@
+﻿<?php
+//Változók
+$eszkoz = $_POST['eszkoz'];
+$id = $_POST['id'];
+
+require('db.php');
+//Ha az a foglalásnál az első dropdown menüben választunk eszközt, akkor biztosan teljesül a feltétel (bár amúgy is), és lefut a következő kód:
+if  ($eszkoz != "na") {
+		echo ' 
+		<div class="tab">
+			<label for="">Eszköz típusa:</label><br>
+			<select id="id" onchange="foglalas('."'".$eszkoz."'".',document.getElementById('."'id'".').value);" name="" class="select">';
+				//Ha még nincs ID (tehát nem történt típus választás), akkor megkérjük a választásra
+				if ($id == "na") {echo '<option value="" disabled selected>Kérlek válassz!</option>';}
+			
+				$query = "SELECT * FROM `devices` WHERE name='$eszkoz'";
+				$result = mysqli_query($con, $query) or die(mysql_error());
+				while ($row = $result->fetch_assoc()) {
+					//A típus kiválasztása után, a hozzátartozó ID alapján újratöltésnél mindig visszaállítjuk a dropdown menüben az előzőleg választottatat egy "selected" tulajdonsággal
+					if ($id == $row['id']) {
+						echo '<option selected="selected" value="'.$row["id"].'">'.$row["type"].' (id: '.$row["id"].')</option> ';
+					} else {
+						echo '<option value="'.$row["id"].'">'.$row["type"].' (id: '.$row["id"].')</option>';
+					}
+				}
+				
+		echo '
+			</select>
+		</div>';}
+		
+//Az eszköz választásnál egyelőre ennek "na" (nincs adat) értéket adunk, így nem fut le. A típus kiválasztásával viszont átadjuk az ID -t is, ami egészen biztos nem "na" (vagy bármi más) és az alapján kérdezzük le az időtartamot
+if  ($id != "na") {
+		echo ' 
+		<div class="tab">
+			<label for="">Foglalás időtartama:</label><br>
+			<select id="" name="" class="select">
+				<option value="" disabled selected>Kérlek válassz!</option>';
+				$query = "SELECT * FROM `devices` WHERE id='$id'";
+				$result = mysqli_query($con, $query) or die(mysql_error());
+				$row = mysqli_fetch_assoc($result);
+				//Itt ciklussal feltöltjük a dropdown menüt, ameddig maximum bérelhető az eszköz
+				$i = 1;
+				while ($i <= $row[period_days]) {
+				echo '<option value="'.$i.'">'.$i.' nap</option>';
+				$i++;}
+		echo '
+			</select>
+		</div>'; }
+
+	  
+
+		
+?>
